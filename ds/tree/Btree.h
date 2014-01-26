6da017f62b5e9a03e469cdc417f995d168e492fb
alexoner <onerhao@gmail.com>
2014-01-26 10:19:47 +0800

    ds:added B+ tree and red-black tree empty file

---
+++ ds/tree/Btree.h
@@ -0,0 +1,7 @@
+#ifndef BTREE_H
+#define BTREE_H
+/***************************************************
+ * B-Tree implementation header file
+ * ************************************************/
+
+#endif
---
+++ ds/tree/rbtree.h
@@ -0,0 +1,8 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+/*****************************************
+ * Red-Black tree implementation header file
+ * ***************************************/
+
+
+#endif
