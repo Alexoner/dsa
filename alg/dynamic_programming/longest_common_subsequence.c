2fec8b3a730b121863e5d4479d6148d8e3bb1609
Alexoner <onerhao@gmail.com>
2014-04-08 11:25:08 +0800

    alg:add longest common subsequence solution using dynamic programming

--- alg/dynamic_programming/longest_common_subsequence.c
+++ alg/dynamic_programming/longest_common_subsequence.c
@@ -16,3 +16,75 @@
  * and Y[n- 1] .
  * 3. If x[m] != y[n], then z[k] != y[n] implies that Z is an LCS of X and Y[n-1].
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_lcs()
+{
+
+}
+
+int lcs_length(char *x, int lx, char *y, int ly)
+{
+    int c[lx + 1][ly + 1]; //contains length of an LCS of X and Y
+    int i, j;
+
+    for (j = 0; j < ly + 1; j++)
+    {
+        c[0][j] = 0;
+    }
+    for (i = 0; i < lx + 1; i++)
+    {
+        c[i][0] = 0;
+    }
+
+    //bottom-up X
+    for (i = 1; i <= lx; i++)
+    {
+
+        //bottom-up Y
+        for (j = 1; j <= ly; j++)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                c[i][j] = c[i - 1][j - 1] + 1;
+            }
+            else if (c[i - 1][j] >= c[i ][j - 1])
+            {
+                c[i][j] = c[i - 1][j];
+            }
+            else
+            {
+                c[i ][j ] = c[i][j - 1];
+            }
+        }
+    }
+
+    //print the solution two-dimensional array
+    printf("   \t");
+    for (i = 0; i <= ly; i++)
+    {
+        printf("%c\t", y[i]);
+    }
+    printf("\n");
+    for (i = 0; i <= lx; i++)
+    {
+        printf("%c: ", x[i]);
+        for (j = 0; j <= ly; j++)
+        {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    //print out the longest common subsequence
+    return 0;
+}
+
+int main()
+{
+    char *x = "ABCBDAB", *y = "BDCABA";
+    lcs_length(x, 7, y, 6);
+    return 0;
+}
