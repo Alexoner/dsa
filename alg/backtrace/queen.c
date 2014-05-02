b7269d07cb2bce5172966478a1afa1b14372ee53
Alexoner <onerhao@gmail.com>
2014-05-02 12:19:20 +0800

    alg:add comment to N Queen problem

--- alg/backtrace/queen.c
+++ alg/backtrace/queen.c
@@ -1,48 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#define MAX 8
+#define MAX 14
+/*#define MAX 4*/
 
-int sum=0;
-int queen[MAX]={0};
+int sum = 0;
+int queen[MAX] = {0};
 int check(int n);
 void place(int n);
 void show();
 
+/**
+ * check whether a placement is valid
+ */
 int check(int n)
 {
     int i;
-    for(i=0;i<n;i++)
+    for (i = 0; i < n; i++)
     {
-        if(queen[i]==queen[n]||abs(queen[i]-queen[n])==n-i)
+        if (queen[i] == queen[n] || abs(queen[i] - queen[n]) == n - i)
             return 0;
     }
     return 1;
 }
 
+/**
+ * Solution implemented with recursive backtrack
+ * @ param n:the row index to place a queen into
+ */
 void place(int n)
 {
     int i;
-    if(n==MAX)
+    if (n == MAX)
     {
         show();
         return;
     }
-    for(i=0;i<MAX;i++)
+    for (i = 0; i < MAX; i++)
     {
-        queen[n]=i;
-        if(check(n))
-            place(n+1);
+        queen[n] = i;
+        if (check(n))
+            place(n + 1);
     }
 }
 
+/**
+ * print out the N queen solution
+ */
 void show()
 {
     sum++;
-    int i,j;
-    for(i=0;i<MAX;i++)
+    int i, j;
+    for (i = 0; i < MAX; i++)
     {
-        for(j=0;j<MAX;j++)
-            if(j==queen[i])
+        for (j = 0; j < MAX; j++)
+            if (j == queen[i])
                 printf("1 ");
             else
                 printf("0 ");
@@ -54,6 +66,6 @@ void show()
 int main()
 {
     place(0);
-    printf("there are %d kinds\n",sum);
+    printf("there are %d solutions\n", sum);
     return 0;
 }
