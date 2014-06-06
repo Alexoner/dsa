8564786b32f2d5397f2769b8bf2f08c12b0bf768
Alexoner <onerhao@gmail.com>
2014-06-06 17:20:30 +0800

    add quickselect algorithm implementation

---
+++ alg/sort/quickselect.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../utils.h"
+
+int quickselect(int *a, int p, int r, int i)
+{
+    if (p == r)
+    {
+        return p;
+    }
+    int q, k;
+    /*q = randomized_partition(a, p, r);*/
+    q = partition(a, p, r);
+    k = q - p + 1;
+    if (k == i)
+    {
+        return q;
+    }
+    else if (i < k)
+    {
+        return quickselect(a, p, q - 1, i);
+    }
+    else
+    {
+        return quickselect(a, q + 1, r, i - k);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
