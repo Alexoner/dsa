70b67d553a9ad57001f4c30368353e9e10097b70
Alexoner <onerhao@gmail.com>
2014-08-21 23:50:23 +0800

    alg:add python implementation for string matching with Finite automata and KMP algorithm

--- alg/math/gcd.c
+++ alg/math/gcd.c
@@ -4,35 +4,34 @@
     > Mail: onerhao@gmail.com
     > Created Time: Thu 22 Nov 2012 02:20:14 PM CST
  ************************************************************************/
-int gcd(int x,int y)
+int gcd(int x, int y)
 {
     int t;
-    while(y>0)
+    while (y > 0)
     {
-        if(x>y)
+        if (x > y)
         {
-            t=x%y;
-            x=y;
-            y=t;
+            t = x % y;
+            x = y;
+            y = t;
         }
         else
         {
-            y%=x;
+            y %= x;
         }
     }
     return x;
 }
 
-int gcdn(int *a,int n)
-{//return the greatest common divisor of n numbers in array a
-    int t,i;
-    if(n<1)
+int gcdn(int *a, int n)
+{ // return the greatest common divisor of n numbers in array a
+    int t, i;
+    if (n < 1)
         return 0;
-    t=gcd(a[0],a[1]);
-    for(i=2;i<n;i++)
+    t = gcd(a[0], a[1]);
+    for (i = 2; i < n; i++)
     {
-        t=gcd(t,a[i]);
+        t = gcd(t, a[i]);
     }
     return t;
 }
-
--- alg/string/finiteAutomata.py
+++ alg/string/finiteAutomata.py
@@ -9,35 +9,101 @@ class FA(object):
         """@todo: to be defined1. """
         pass
 
-    def finite_automata_matcher(self, text, tf, m):
+    def finite_automata_matcher(self, T, delta, m):
         """@todo: Docstring for finite_automata_matcher.
 
+        Time Complexity:O(n)
         :text: input text
-        :tf:transition functio
+        :delta:transition function
         :m: the only accepted state
         :returns: @todo
 
         """
-        n = len(text)
+        n = len(T)
         q = 0
         for i in range(n):
-            q = tf[q][text[i]]
+            q = delta[q][T[i]]
             if q == m:
                 print "Pattern occurs at shift {}".format(i - m + 1)
 
-    def compute_transition_function(self, pattern, input_alphabet):
+    def compute_transition_function(self, P, sigma):
         """
         :pattern: given pattern
         :input_alphabet: input alphabet
 
         """
-        m = len(pattern)
-        tf = [[0 for i in range(input_alphabet)] for j in range(m)]
+        m = len(P)
+        # delta:transition funciton
+        delta = [[0 for i in range(sigma)] for j in range(m)]
         for q in range(m):
-            for a in input_alphabet:
+            for a in sigma:
                 k = min(m + 1, q + 2)
                 k = k - 1
-                while not self.is_suffix_of(pattern[0:k], pattern[0:q] + a):
+                while not self.is_suffix_of(P[0:k], P[0:q] + a):
                     k = k - 1
-                tf[q][a] = k
-        return tf
+                delta[q][a] = k
+        return delta
+
+    def is_suffix_of(self, suffix, text):
+        l1 = len(suffix)
+        l2 = len(text)
+        dl = l2 - l1
+        if l1 > l2:
+            return False
+        if l1 == 0:
+            return True
+        for i in xrange(l1):
+            if suffix[i] != text[i + dl]:
+                return False
+
+        return True
+
+    def kmp_matcher(self, T, P):
+        """
+        Time Complexity:O(n)
+        text:input text
+        pattern: pattern text
+
+        KMP algorithm avoids the explicit computation of the
+        transition function DELTA altogether.
+        pi[q] is the length of the longest prefix of P that is a proper
+        suffix of P[0]...P[q]
+        """
+        n = len(T)
+        m = len(P)
+
+        # prefix function
+        pi = self.compute_prefix_function(P)
+        q = 0  # match length
+        for i in range(n):
+            while q > 0 and P[q] != T[i]:
+                q = pi[q - 1]
+            if P[q] == T[i]:
+                q = q + 1
+            if q == m:
+                q = pi[q - 1]
+                print "Pattern occurs with shift {}".format(i - m + 1)
+
+    def compute_prefix_function(self, P):
+        """
+        Time Complexity:O(m)
+        Prefix function has the property of recurrence.
+        """
+
+        m = len(P)
+        pi = [0 for i in range(m)]
+        k = 0  # the match length
+        for q in range(1, m, 1):
+            while k > 0 and P[k] != P[q]:
+                k = pi[k - 1]
+            if P[k] == P[q]:
+                k = k + 1
+            pi[q] = k
+
+        return pi
+
+if __name__ == "__main__":
+    fa = FA()
+    fa.kmp_matcher("ABABDABACDABABCABAB", "ABABCABAB")
+    fa.kmp_matcher("AABAACAADAABAAABAA", "AABA")
+    fa.kmp_matcher("THIS IS A TEST TEXT", "TEST")
