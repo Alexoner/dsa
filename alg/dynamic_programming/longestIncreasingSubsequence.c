0c183b7348347bd3bbc41e7276863964449a8af9
Alexoner <onerhao@gmail.com>
2014-08-31 12:09:25 +0800

    alg:Dynamic Programming Solution to Longest Increasing Subsequence problem

--- alg/dynamic_programming/longestIncreasingSubsequence.c
+++ alg/dynamic_programming/longestIncreasingSubsequence.c
@@ -17,3 +17,82 @@
  * So the LIS problem has optimal substructure property as the main
  * problem can be solved using solutions to subproblems.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) (a) >= (b) ? (a) : (b)
+
+int lis(int *a, int n)
+{
+    // length[i]:the length of LIS with a[i] as the last element
+    int length[n];
+    int max_length = 1;
+    int end;
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        length[i] = 1;
+        for (j = 0; j < i; j++)
+        {
+            if (a[j] <= a[i])
+            {
+                length[i] = MAX(length[i], length[j] + 1);
+                if (max_length < length[i])
+                {
+                    max_length = length[i];
+                    end = i;
+                }
+            }
+        }
+    }
+
+    return max_length;
+}
+
+int lis_print_solution(int *a, int n)
+{
+    // length[i]:the length of LIS with a[i] as the last element
+    int length[n];
+    int max_length = 1;
+    int end;
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        length[i] = 1;
+        for (j = 0; j < i; j++)
+        {
+            if (a[j] <= a[i])
+            {
+                length[i] = MAX(length[i], length[j] + 1);
+                if (max_length < length[i])
+                {
+                    max_length = length[i];
+                    end = i;
+                }
+            }
+        }
+    }
+    int is[max_length];
+    for (i = end, j = max_length - 1; i + 1 && j + 1; j--)
+    {
+        is[j] = i;
+        printf("%d ", a[i]);
+        for (; i + 1 && length[i] != length[is[j]] - 1; i--)
+            ;
+    }
+    printf("\n");
+    for (i = 0; i < max_length; i++)
+        printf("%d ", a[is[i]]);
+    printf("\n");
+    return max_length;
+}
+
+int main(int argc, char **argv)
+{
+    int arr[] = { 10, 22, 9, 33, 21, 50, 41, 80, 60, 30 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Length of LIS is %d\n", lis_print_solution(arr, n));
+    /*getchar();*/
+    return 0;
+}
