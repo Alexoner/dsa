5a370bc73e25588dfc49a5fa87b1d497f3c2e06f
Alexoner <onerhao@gmail.com>
2014-09-09 15:07:14 +0800

    alg:change binary search function prototype to contain left and right index

--- alg/search/binmain.c
+++ alg/search/binmain.c
@@ -4,10 +4,14 @@
 int main()
 {
     int a[] = { 2, 3, 5, 7, 8, 10, 12, 15, 19, 21 }, i, key;
+    int n = sizeof(a) / sizeof(a[0]);
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
     printf("please input the number you want to query\n");
     while (scanf("%d", &key))
     {
-        i = binsearch(a, 10, key);
+        i = binsearch(a, 0, n - 1, key);
         if (i != -1)
         {
             printf("%d is the %dst one\n", key, i + 1);
--- alg/search/binsearch.c
+++ alg/search/binsearch.c
@@ -4,18 +4,16 @@
 /*
  * Time Complexity:O(log(N))
  */
-int binsearch(int *a, int n, int key)
+int binsearch(int *a, int left, int right, int key)
 {
-    int low, high, mid;
-    low = 0;
-    high = n - 1;
-    while (low <= high)
+    int mid;
+    while (left <= right)
     {
-        mid = (low + high) >> 1;
+        mid = (left + right) >> 1;
         if (a[mid] > key)
-            high = mid - 1; // recursion .Don't forget "-1"!
+            right = mid - 1; // recursion .Don't forget "-1"!
         else if (a[mid] < key)
-            low = mid + 1;
+            left = mid + 1;
         else
             return mid;
     }
