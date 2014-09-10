54bc6a4b572c6cc8758cfbaeb69896f00ac7e626
Alexoner <onerhao@gmail.com>
2014-09-10 10:48:21 +0800

    alg:another DP solution,two round of bottom up

--- alg/dynamic_programming/longestBitonicSubsequence.c
+++ alg/dynamic_programming/longestBitonicSubsequence.c
@@ -129,10 +129,9 @@ int lbs_print_solution(int arr[], int n)
         }
     }
 
-    for (i = 0; i < n; i++)
-        printf("%d ", lis[i]);
-    printf("\n");
-    /*printf("\npeak: %d,end: %d\n", peak, end);*/
+    /*for (i = 0; i < n; i++)*/
+    /*printf("%d ", lis[i]);*/
+    /*printf("\n");*/
 
     int bs[max_lbs];
     bs[max_lbs - 1] = end;
@@ -173,14 +172,52 @@ int lbs_print_solution(int arr[], int n)
     lis[i] ==> Longest Increasing subsequence ending with arr[i]
     lds[i] ==> Longest decreasing subsequence starting with arr[i]
 */
+int lbs2(int arr[], int n)
+{
+    int i, j;
+
+    /* Allocate memory for LIS[] and initialize LIS values as 1 for
+       all indexes */
+    /*int *lis = new int[n];*/
+    int lis[n];
+    for (i = 0; i < n; i++)
+        lis[i] = 1;
+
+    /* Compute LIS values from left to right */
+    for (i = 1; i < n; i++)
+        for (j = 0; j < i; j++)
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                lis[i] = lis[j] + 1;
+
+    /* Allocate memory for lds and initialize LDS values for
+       all indexes */
+    /*int *lds = new int[n];*/
+    int lds[n];
+    for (i = 0; i < n; i++)
+        lds[i] = 1;
+
+    /* Compute LDS values from right to left */
+    for (i = n - 2; i >= 0; i--)
+        for (j = n - 1; j > i; j--)
+            if (arr[i] > arr[j] && lds[i] < lds[j] + 1)
+                lds[i] = lds[j] + 1;
+
+    /* Return the maximum value of lis[i] + lds[i] - 1*/
+    int max = lis[0] + lds[0] - 1;
+    for (i = 1; i < n; i++)
+        if (lis[i] + lds[i] - 1 > max)
+            max = lis[i] + lds[i] - 1;
+    return max;
+}
 
 int main(int argc, char **argv)
 {
-    /*int arr[] = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };*/
+    int arr[] = { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 };
     /*int arr[] = { 80, 60, 30, 40, 20, 10 };*/
-    int arr[] = { 1, -1, 3, 2, -3 };
+    /*int arr[] = { 1, -1, 3, 2, -3 };*/
     /*int arr[] = { 1, 2, 3, 4, 5, 6 };*/
     int n = sizeof(arr) / sizeof(arr[0]);
     printf("Length of LBS is %d\n", lbs_print_solution(arr, n));
+    printf("Length of LBS is %d\n", lbs2(arr, n));
     return 0;
 }
