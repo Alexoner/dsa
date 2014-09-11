2854001fe2917aa053b468c9bfa98cc1519e473e
Alexoner <onerhao@gmail.com>
2014-09-11 09:36:56 +0800

    alg:space optimized from n*n to n

--- alg/dynamic_programming/coinChange.c
+++ alg/dynamic_programming/coinChange.c
@@ -21,6 +21,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+ * Time Complexity:O(m*n)
+ */
 int coinChange(int S[], int m, int n)
 {
     int table[m][n + 1];
@@ -41,11 +44,35 @@ int coinChange(int S[], int m, int n)
     return table[m - 1][n];
 }
 
+int coinChange_space_opt(int S[], int m, int n)
+{
+    // table[i] will be storing the number of solutions for
+    // value i. We need n+1 rows as the table is consturcted
+    // in bottom up manner using the base case (n = 0)
+    int table[n + 1];
+
+    // Initialize all table values as 0
+    memset(table, 0, sizeof(table));
+
+    // Base case (If given value is 0)
+    table[0] = 1;
+
+    // Pick all coins one by one and update the table[] values
+    // after the index greater than or equal to the value of the
+    // picked coin
+    for (int i = 0; i < m; i++)
+        for (int j = S[i]; j <= n; j++)
+            table[j] += table[j - S[i]];
+
+    return table[n];
+}
+
 int main(int argc, char **argv)
 {
     int arr[] = { 1, 2, 3 };
     int m = sizeof(arr) / sizeof(arr[0]);
     int n = 4;
     printf("%d\n", coinChange(arr, m, n));
+    printf("%d\n", coinChange_space_opt(arr, m, n));
     return 0;
 }
