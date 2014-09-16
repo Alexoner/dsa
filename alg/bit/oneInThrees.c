22399d6e52ee8c1942fad5b01bd02aa61dc2d8bb
Alexoner <onerhao@gmail.com>
2014-09-16 22:53:21 +0800

    alg:bitwise algorithm for Single Number problem

--- alg/bit/oneInThrees.c
+++ alg/bit/oneInThrees.c
@@ -21,20 +21,39 @@ int oneInThrees(int arr[], int n)
     {
         three ^= two & arr[i]; // XOR,elements appear 3 times
         two ^= one & arr[i];   // elements appear 2 times
-        one ^= arr[i];         // elements that appear even times so far
+        one ^= arr[i];         // bits that appear even times
         /*if ((three & arr[i]) != arr[i])*/
         /*one ^= arr[i];*/
-        one = one & ~three; // elements that appear 1 time so far
-                            /*printf("%d,%d,%d\n", one, two, three);*/
+        one = one ^ three;
+        printf("%d,%d,%d\n", one, two, three);
     }
     return one;
+    /*return three;*/
+}
+
+int oneInThrees2(int arr[], int n)
+{
+    int ones = 0, twos = 0;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        /*twos ^= ones & arr[i];*/
+        twos |= ones & arr[i];
+        ones ^= arr[i];
+
+        int mask = ~(ones & twos);
+        ones &= mask;
+        twos &= mask;
+    }
+    return ones;
 }
 
 int main(int argc, char **argv)
 {
-    /*int arr[] = { 12, 1, 12, 3, 12, 1, 1, 2, 2, 3, 3 };*/
-    int arr[] = { 3, 2, 3, 3 };
+    int arr[] = { 12, 1, 12, 3, 12, 1, 1, 2, 3, 3 };
+    /*int arr[] = { 3, 2, 3, 3 };*/
     int n = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", oneInThrees(arr, n));
+    printf("oneInThrees: %d\n", oneInThrees(arr, n));
+    printf("oneInthrees2: %d\n", oneInThrees2(arr, n));
     return 0;
 }
