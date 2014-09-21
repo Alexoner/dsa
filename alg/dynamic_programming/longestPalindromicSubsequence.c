b9be1f4e2dcbd96e718450e80f1c47c113cf645b
Alexoner <onerhao@gmail.com>
2014-09-21 14:16:32 +0800

    alg:add Longest Palindromic Subsequence problem

---
+++ alg/dynamic_programming/longestPalindromicSubsequence.c
@@ -0,0 +1,10 @@
+/*
+ * Dynamic Programming | Longest Palindromic Subsequence
+ *
+ * Given a sequence, find the length of the longest palindromic subsequence
+ * in it. For example, if the given sequence is “BBABCBCAB”, then the
+ * output should be 7 as "BABCBAB" is the longest palindromic subseuqnce in
+ * it. "BBBBB" and "BBCBB" are also palindromic subsequences of the given
+ * sequence, but not the longest ones.
+ *
+ */
