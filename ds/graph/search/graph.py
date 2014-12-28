afbe22c9d17a6b7624c6211ba863d53784c4806c
Alexoner <onerhao@gmail.com>
2014-12-28 16:33:08 +0800

    ds.graph:add Breadth-first search with queue implementation

--- ds/graph/search/graph.py
+++ ds/graph/search/graph.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python2
 
+import sys
+import Queue
+
 
 class Graph:
 
-    def __init__(self,vertices,edges):
+    def __init__(self, vertices, edges):
         self.V = []     # list of vertex
         self.E = []     # list of edge,
 
@@ -11,20 +14,55 @@ class Graph:
 
         self.adj = []       # adjacency-list representation
 
+    def BFS(G, s):
+        """
+        Breadth-first search.
+        QUEUE.
+        """
+
+        # initialization
+        # for vertex in G.V:
+        for u in G.V:
+            u.color = WHITE
+            u.d = sys.maxint
+            u.predecessor = None
+
+        s.color = GRAY
+        s.d = 0
+
+        Q = Queue.Queue()
+        Q.put(s)
+
+        # maintenance
+        # Elements in the QUEUE are the ones that have NOT been processed
+        # POP it out of the QUEUE and visit it,and PUSH its successors
+        # into the queue
+        while not Q.empty():
+            u = Q.get()
+            for v in G.adj[u]:
+                if v.color == WHITE:
+                    v.color = GRAY
+                    v.d = u.d + 1
+                    v.predecessor = u
+                    Q.put(v)
+            u.color = BLACK
 
     def DFS(G):
         '''
-            Depth-first search.Recursion (Stack).
+            Depth-first search.
+            Recursion (Stack).
         '''
-        for vertex in G.V:
+
+        # Initialization
+        for u in G.V:
             u.color = WHITE
             u.predecessor = None
         time = 0
 
-        for vertex in G.V:
+        # Maintenance
+        for u in G.V:
             if u.color == WHITE:
                 DFS_VISIT(G, u)
-
 
     def DFS_VISIT(G, u):
         time = time + 1     # time stamp
@@ -34,7 +72,7 @@ class Graph:
         for v in G.adj[u]:
             if v.color == WHITE:    # WHITE: unvisited
                 v.predecessor = u
-                DFS_VISITT(G,v)
+                DFS_VISITT(G, v)
 
         # time stamp increased by 1,representing the finishing time
         u.color = BLACK     # BLACK: visited
